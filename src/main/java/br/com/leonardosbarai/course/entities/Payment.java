208604b9b6393ed4fdb73bc85b57d2bdd64b2391
Leonardo Sbarai <leonardo.sbarai@gmail.com>
2020-01-08 21:51:12 -0300

    Subtotal & Total methods

--- src/main/java/br/com/leonardosbarai/course/entities/Payment.java
+++ src/main/java/br/com/leonardosbarai/course/entities/Payment.java
@@ -1,5 +1,7 @@
 package br.com.leonardosbarai.course.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import javax.persistence.*;
 import java.time.Instant;
 import java.util.Objects;
@@ -14,6 +16,7 @@ public class Payment {
     private Long id;
     private Instant moment;
 
+    @JsonIgnore
     @OneToOne
     @MapsId
     private Order order;
